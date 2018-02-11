a6afff4b79b01bf4371e907b02f1be9fbc6d3bd3
Christopher Coleman <mellowfish@fastmail.fm>
2018-02-11 11:14:47 -0600

    More abstract work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,38 @@ module ApplicationHelper
   def external_link_to(*args, **options, &block)
     link_to(*args, **options.merge(target: "_blank"), &block)
   end
+
+  # https://www.youtube.com/watch?v=umnsJwdU1Og
+  # https://youtu.be/umnsJwdU1Og
+  def youtube_url?(url)
+    uri = URI.parse(url)
+    case uri.host
+    when "www.youtube.com"
+      uri.path == "/watch" && uri.query.include?("v=")
+    when "youtu.be"
+      true
+    else
+      false
+    end
+  end
+
+  def card_youtube_iframe(card)
+    uri = URI.parse(card.url)
+
+    video_id =
+      case uri.host
+      when "www.youtube.com"
+        Rack::Utils.parse_nested_query(uri.query).symbolize_keys[:v]
+      when "youtu.be"
+        uri.path
+      else
+        nil
+      end
+
+    return nil if video_id.blank?
+
+    <<-HTML.html_safe
+      <iframe id="ytplayer" type="text/html" width="640" height="360" src="https://www.youtube.com/embed/#{video_id}?autoplay=0&origin=#{card_url(card)}" frameborder="0"></iframe>
+    HTML
+  end
 end
